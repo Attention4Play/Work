0713bb0f858a55e7b01033ae9d7f3dcc0424012b
Attention4Play <andrew.filippov2010@yandex.ru>
2025-04-06 22:23:53 +0300

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Person person = new Person();
-//        System.out.print("Введите ваш ник: ");
-//        person.setUserName(scan.nextLine());
-//        System.out.print("Введите ваш логин: ");
-//        person.setLogin(scan.nextLine());
-//        System.out.println(person);
+        System.out.print("Введите ваш ник: ");
+        person.setUserName(scan.nextLine());
+        System.out.print("Введите ваш логин: ");
+        person.setLogin(scan.nextLine());
+        System.out.println(person);
         PersonService personService = new PersonService();
         personService.generateFakePersons();
     }
